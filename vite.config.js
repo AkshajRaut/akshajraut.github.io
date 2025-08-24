382c9958b2790a5785e4a058bc3890fdad4b4661
Akshaj <akshajraut@gmail.com>
2025-08-23 20:49:46 -0400

    Update

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// If repo is username.github.io → use '/'
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
